0e3c15982baaee4ec4a6540599e55f3851b76328
YaRkOshARkO <gurlibob@gmail.com>
2021-01-28 19:56:20 -0500

    Initial commit - Thursday 8:00PM

---
+++ src/LoginFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class LoginFrame {
+
+    public static void create(String args[]) {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        frame.setSize(245, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
